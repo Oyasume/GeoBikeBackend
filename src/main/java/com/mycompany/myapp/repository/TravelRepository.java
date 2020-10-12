c148d5e7567a6cb7510f7f64257f845f5f328f3e
Diego <romero.diego31@gmail.com>
2020-10-12 15:55:40 +0200

    travel en fonction du user

--- src/main/java/com/mycompany/myapp/repository/TravelRepository.java
+++ src/main/java/com/mycompany/myapp/repository/TravelRepository.java
@@ -3,9 +3,11 @@ package com.mycompany.myapp.repository;
 import com.mycompany.myapp.domain.Travel;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the Travel entity.
@@ -16,4 +18,16 @@ public interface TravelRepository extends JpaRepository<Travel, Long> {
 
     @Query("select travel from Travel travel where travel.user.login = ?#{principal.username}")
     List<Travel> findByUserIsCurrentUser();
+
+    @Query("select distinct travel from Travel travel"
+        + " left join fetch travel.locations"
+        + " where travel.user.login = ?#{principal.username}"
+    )
+    List<Travel> findByUserIsCurrentUserWithEagerRelationships();
+
+    @Query("select travel from Travel travel"
+    + " left join fetch travel.locations"
+    + " where travel.id = :id"
+    )
+    Optional<Travel> findByIdWithPositions(@Param("id") Long id);
 }
